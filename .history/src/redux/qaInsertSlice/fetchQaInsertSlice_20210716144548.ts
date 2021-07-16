3ed181e9cff0b75b94fcf553f29edf2a699740cd
nyuakimine <79794028+nyuakimine@users.noreply.github.com>
2021-07-17 02:13:09 +0900

    2021/07/17

--- .history/src/redux/qaInsertSlice/fetchQaInsertSlice_20210716144548.ts
+++ .history/src/redux/qaInsertSlice/fetchQaInsertSlice_20210716144548.ts
@@ -1,50 +1,47 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-interface NewBeeMallProductQaPagingState {
+interface NewBeeMallProductQaInsertState {
   loading: boolean;
   error: string | null;
   data: {totalCount:number,pageSize:number,totalPage:number,currPage:number,list:any[]};
-  page: any;
+
 }
 
-const initialState: NewBeeMallProductQaPagingState = {
+const initialState: NewBeeMallProductQaInsertState = {
   loading: true,
   error: null,
   data:  {totalCount:0,pageSize:3,totalPage:0,currPage:1,list:[]},
-  page: 1,
+
 };
 
-export const fetchQaPagingDataActionCreator = createAsyncThunk(
-  "qaPageSlice/fetchQaPagingDataActionCreator",
-  async (page: number, thunkAPI) => {
-    const { data } = await axios.post(`http://localhost:8081/qaPaging`, {
-      page: page,
-    });
+export const fetchQaInsertDataActionCreator = createAsyncThunk(
+  "qaInsertSlice/fetchQaInsertDataActionCreator",
+  async (question: any, thunkAPI) => {
+    const { data } = await axios.post(`http://localhost:8081/insertQa`, question
+    );
     return data;
   }
 );
-export const newBeeMallQaPagingSlice = createSlice({
-  name: "qaPageSlice",
+export const newBeeMallQaInsertSlice = createSlice({
+  name: "qaInsertSlice",
   initialState,
   reducers: {},
   extraReducers: {
-    [fetchQaPagingDataActionCreator.pending.type]: (state) => {
+    [fetchQaInsertDataActionCreator.pending.type]: (state) => {
       // return { ...state, loading: true };
       state.loading = true;
     },
-    [fetchQaPagingDataActionCreator.fulfilled.type]: (state, action) => {
-      //debugger;
-      console.log("lllllllQaPaging", action.payload);
+    [fetchQaInsertDataActionCreator.fulfilled.type]: (state, action) => {
+     // debugger;
+      console.log("lllllllQaInsert", action.payload);
       state.data = action.payload.data;
-      state.page = action.payload.data.currPage;
-      console.log("wwwwwwwwwwwwwwwwQaPaging",state.page);
        //state.page = action.payload.page;
 
       state.loading = false;
       state.error = null;
     },
-    [fetchQaPagingDataActionCreator.rejected.type]: (
+    [fetchQaInsertDataActionCreator.rejected.type]: (
       state,
       action: PayloadAction<string | null>
     ) => {
